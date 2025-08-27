71ee81c040a4f2df2a926c42e0eec8f69434fe0f
Napaping <napapingvz@gmail.com>
2025-08-28 00:10:43 +0700

    Test Swagger

--- go-sample-api/cmd/api/main.go
+++ go-sample-api/cmd/api/main.go
@@ -7,7 +7,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 // สร้างตัวแปรกำหนด port ที่จะใช้
@@ -26,22 +29,69 @@ type application struct {
 	APIKey       string
 }
 
+// @title Movies API with GO and PostgreSQL
+// @version 1.0
+// @description This is a Movies API with GO and PostgreSQL
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 func main() {
 
 	// Set application config
 	var app application
 	app.Domain = "example.com"
 
+	// โหลดค่าจากไฟล์ .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// สร้าง DSN สำหรับเชื่อมต่อฐานข้อมูล
+	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s timezone=%s connect_timeout=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"),
+		os.Getenv("DB_CONNECT_TIMEOUT"),
+	)
+
+	// อ่านค่าจาก environment variables
+	app.DSN = dsn
+
 	// Read from command line arguments
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 dbname=gosampledb user=postgres password=123456 sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	// flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 dbname=gosampledb user=postgres password=123456 sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+
+	// ตั้งค่าตัวแปร JWT จาก environment variables
+	app.JWTSecret = os.Getenv("JWT_SECRET")
+	app.JWTIssuer = os.Getenv("JWT_ISSUER")
+	app.JWTAudience = os.Getenv("JWT_AUDIENCE")
+	app.CookieDomain = os.Getenv("COOKIE_DOMAIN")
+	app.Domain = os.Getenv("DOMAIN")
+	app.APIKey = os.Getenv("API_KEY")
 
 	// Parse the command line arguments for JWT
-	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
-	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
-	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
-	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
-	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
-	flag.StringVar(&app.APIKey, "api-key", "b41447e6319d1cd467306735632ba733", "api key")
+	// flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	// flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	// flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	// flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	// flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	// flag.StringVar(&app.APIKey, "api-key", "b41447e6319d1cd467306735632ba733", "api key")
 
 	flag.Parse()
 
--- go-sample-api/cmd/api/routes.go
+++ go-sample-api/cmd/api/routes.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	_ "backend/cmd/api/docs" // เปลี่ยนชื่อโมดูลให้ตรงกับชื่อใน go.mod
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func (app *application) routes() http.Handler {
@@ -16,25 +18,44 @@ func (app *application) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(app.enableCORS)
 
-	// register routes
-	mux.Get("/", app.Home)
-	mux.Get("/about", app.About)
-	mux.Get("/demomovies", app.AllDemoMovies)
+	// Register Swagger
+	mux.Get("/swagger/*", httpSwagger.WrapHandler)
 
-	// Authenticated routes
-	mux.Post("/authenticate", app.authenticate)
-	mux.Get("/refresh", app.refreshToken)
-	mux.Get("/logout", app.logout)
+	// Register the API routes under "/api/v1"
+	mux.Route("/api/v1", func(r chi.Router) {
+		// Public routes
+		r.Get("/", app.Home)
+		r.Get("/about", app.About)
+		r.Get("/demomovies", app.AllDemoMovies)
 
-	mux.Get("/movies", app.AllMovies)
-	mux.Get("/movies/{id}", app.GetMovie)
+		// Authenticated routes
+		r.Post("/authenticate", app.authenticate)
+		r.Get("/refresh", app.refreshToken)
+		r.Get("/logout", app.logout)
 
-	mux.Route("/admin", func(mux chi.Router) {
+		//Protected routes
+		r.With(app.jwtMiddleware).Get("/admin/movies", app.MovieCatalog)
+		r.With(app.jwtMiddleware).Get("/admin/movies/{id}", app.MovieForEdit)
+		r.With(app.jwtMiddleware).Post("/admin/movies", app.InsertMovie)
+		r.With(app.jwtMiddleware).Put("/admin/movies/{id}", app.UpdateMovie)
+		r.With(app.jwtMiddleware).Delete("/admin/movies/{id}", app.DeleteMovie)
 
-		// Protected routes
-		mux.Use(app.authRequired)
+		r.Get("/movies", app.AllMovies)
+		r.Get("/movies/{id}", app.GetMovie)
+		r.Get("/genres", app.AllGenres)
 
-		mux.Get("/movies/{id}", app.MovieForEdit)
+		// Admin routes
+		r.Route("/admin", func(r chi.Router) {
+
+			// Protected routes
+			r.Use(app.authRequired)
+
+			r.Get("/movies", app.MovieCatalog)
+			r.Get("/movies/{id}", app.MovieForEdit)
+			r.Post("/movies", app.InsertMovie)
+			r.Put("/movies/{id}", app.UpdateMovie)
+			r.Delete("/movies/{id}", app.DeleteMovie)
+		})
 	})
 
 	return mux
